d45d376c0d8bd40f1ac88d0b65d67e104bd5a333
Firas <firassoekhai@hotmail.nl>
2020-10-20 10:34:23 +0200

    toets gemaakt

--- toets/opdracht 1/specs.spec.js
+++ toets/opdracht 1/specs.spec.js
@@ -1,26 +1,28 @@
-describe('Testje 1', function() {
-    it('should have the correct title', function () {
-        expect(document.title).toBe('Voorbeeld toets');
+describe('Testen voor goede titel en UTF-8.', function() {
+    it('Het zou de goede titel moeten zijn met correcte UTF-8.', function () {
+        expect(document.title).toBe('Verbeteringen');
         expect(document.charset).toBe('UTF-8');
     });
 });
 
-describe('Testje 2', function () {
-    it('should test', function () {
-        expect(unitTest.item).toBe('voorbeeld');
-        it('should test', function () {
-            expect(unitTest.nummer).toEqual(3);
-        });
+describe('Testen of unitTest.item gelijk is aan "item". ', function () {
+    it('Zou item moeten zijn.', function () {
+        expect(unitTest.item).toBe('item');
     });
 });
 
-describe('Testje 3', function () {
-    it('should test', function () {
+describe('Testen of unitTest.beschrijving gelijk is aan "Beschrijving".', function () {
+    it('Zou Beschrijving moeten zijn.', function () {
         expect(unitTest.beschrijving).toBe('Beschrijving');
-        describe('Testje 4', function () {
-            it('should test', function () {
-                expect(unitTest.boodschap).toEqual('Hallo');
-            });
-        });
+    });
+});
+describe('Testen of unitTest.boodschap gelijk is aan "Hallo".', function () {
+    it('Zou Hallo moeten zijn.', function () {
+        expect(unitTest.boodschap).toEqual('Hallo');
+    });
+});
+describe('Testen of unitTest.nummer gelijk is aan de nummer "2".', function () {
+    it('Zou 2 moeten zijn.', function () {
+        expect(unitTest.nummer).toEqual(2);
     });
 });
--- toets/opdracht 2/winkelwagen.js
+++ toets/opdracht 2/winkelwagen.js
@@ -1,5 +1,50 @@
+function Broek(merk, maat) {
+    this.getMerk = function () {
+        return merk;
+    }
+   
+    this.getMaat = function () {
+        return maat;
+    }
+   
+    this.showDetails = function () {
+        console.log(`Broek merk: ${merk} Broekmaat: ${maat}.`);
+    }
+   
+}
 
+function Voorraad(item, aantal) {
+    let artikel = item;
+    let voorraad = aantal;
 
+    this.getVoorraad = function () {
+        return voorraad;
+    }
+    this.getArtikel = function () {
+        return artikel;
+    }
+    this.showVoorraad = function () {
+        console.log(`De voorraad van: ${artikel} is ${voorraad}.`);
+    }
+    this.verlaagVoorraad = function () {
+        voorraad = voorraad  -1;
+    }
 
+    this.verhoogVoorraad = function () {
+        voorraad = voorraad  +1;
+    }
 
+    this.verhoogMeerdereVoorraad = function () {
+        voorraad = voorraad  +5;
+    }
 
+    this.verlaagMeerdereVoorraad = function () {
+        voorraad = voorraad  -5;
+    }
+}
+document.getElementById('stuurBroek').addEventListener('click', () => {
+    let merk = document.getElementById('broekMerk').value;
+    let maat = document.getElementById('broekMaat').value;
+
+    newBroekObject = new Broek(merk, maat);
+});
